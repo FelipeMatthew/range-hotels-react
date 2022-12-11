2840034c2c3a9fdc29f102836d6e726b84294434
Felipe Matthew Moreira Nascimento <felipematthewnascimento@gmail.com>
2022-12-11 13:51:13 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LandingPage from './pages/LandingPage';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
